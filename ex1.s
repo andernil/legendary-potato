4c6c9878c5d15b5b938c0d737043edd203e9bdde
andernil <annil@stud.ntnu.no>
2017-09-12 13:39:33 +0200

    Working interrupt version similar to the polling one. Removed loading of words and big integers and instead load them directly when needed

--- ex1.s
+++ ex1.s
@@ -74,17 +74,16 @@
 	/////////////////////////////////////////////////////////////////////////////
 	//
 	// Reset handler
-  // The CPU will start executing here after a reset
+ 	// The CPU will start executing here after a reset
 	//
 	/////////////////////////////////////////////////////////////////////////////
 	      .globl  _reset
 	      .type   _reset, %function
         .thumb_func
 _reset: 
-	////////////////////////////////////////
 	//Enable CMU for GPIO
 	// load CMU base address
-	ldr r2, cmu_base_addr
+	ldr r2, =CMU_BASE
 	
 	// load current value of HFPERCLK ENABLE
 	ldr r3, [r2,#CMU_HFPERCLKEN0]
@@ -96,80 +95,81 @@ _reset:
 
 	// store new value
 	str r3, [r2, #CMU_HFPERCLKEN0]
-	
-	///////////////////////////////////////////
-	//Loaded addresses and values
-	cmu_base_addr:
-	            .long CMU_BASE
-	gpio_pa_base_addr:
-	            .long GPIO_PA_BASE
-	gpio_pc_base_addr:
-	            .long GPIO_PC_BASE
-	enable_LEDs:
-	            .long 0x55555555
-	enable_GPIO_IN:
-	            .long 0x33333333
-	
-	///////////////////////////////////////////
+
+
 	//Enable LEDs (pins 8-15)
 	// set high drive strength for LEDs by writing 0x2 to GPIO_PA_CTRL
-	ldr r0, gpio_pa_base_addr   //DON'T USE THIS REGISTER AGAIN
+	ldr r0, =GPIO_PA_BASE  //DON'T USE THIS REGISTER AGAIN
 	mov r3, #2
 	str r3, [r0, #GPIO_CTRL]
 	
 	//set pins 8-15 to output by writing 0x55555555 to GPIO_PA_MODEH
-	ldr r3, enable_LEDs
+	ldr r3, =0x55555555
 	str r3, [r0, #GPIO_MODEH]
 	
 	//pins 8-15 can be set high or low by writing to bits 8-15 of GPIO_PA_DOUT
 	
-	
-	/////////////////////////////////////////////
+
 	//Enable inputs for GPIO (pins 0-7)
 	// Set pins 0-7 to input by writing 0x33333333 to GPIO_PC_MODEL
-	ldr r3, enable_GPIO_IN
-	ldr r1, gpio_pc_base_addr   //DON'T USE THIS REGISTER AGAIN
+	ldr r3, =0x33333333
+	ldr r1, =GPIO_PC_BASE   //DON'T USE THIS REGISTER AGAIN
 	str r3, [r1, #GPIO_MODEL]
 	
 	// Enable internal pull-up by writing 0xff to GPIO_PC_DOUT (0xff = 255)
 	mov r3, #255
 	str r3, [r1, #GPIO_DOUT]
-	
 	//status of pins 0-7 can be found by reading GPIO_PC_DIN
 	
-	b label1
+
+	//Enable interrupt handling NVIC
+	ldr r4, =ISER0		
+	ldr r5, [r4]
+	movw r3, #0x802		
+	orr r3, r3, r5
+	str r3, [r4, #0]
+
+
+	//GPIO interrupts
+	ldr r4, =GPIO_BASE
+	ldr r3, =0x22222222
+	str r3, [r4, #GPIO_EXTIPSELL]
 	
-	// BEGIN LOOP
-	label1:
+	//Set interrupt on pos and neg slope aswell as interrupt generation
+	mov r3, #0xFF
+	str r3, [r4, #GPIO_EXTIFALL] 
+	str r3, [r4, #GPIO_EXTIRISE]
+	str r3, [r4, #GPIO_IEN]
 
-	ldr r4, [r1, #GPIO_DIN]
-	lsl r4, r4, #8
-	str r4, [r0, #GPIO_DOUT]
-	
-	b label1
-	
-	
+	//Enable deep-sleep
+	ldr r4, =SCR
+	mov r3, #6
+	str r3, [r4, #0]
 
-
-
-
+	//Initiate deep-sleep
+	wfi
 
 	
 	/////////////////////////////////////////////////////////////////////////////
 	//
-  // GPIO handler
-  // The CPU will jump here when there is a GPIO interrupt
+  	// GPIO handler
+  	// The CPU will jump here when there is a GPIO interrupt
 	//
 	/////////////////////////////////////////////////////////////////////////////
 	
         .thumb_func
 gpio_handler:  
+	//Clear interrupts
+	ldr r1, =GPIO_PC_BASE
+	ldr r2, =GPIO_BASE
+	mov r3, #255
+	str r3, [r2, #GPIO_IFC]
+	
+	ldr r4, [r1, #GPIO_DIN]
+	lsl r4, r4, #8
+	str r4, [r0, #GPIO_DOUT]
+	bx LR
 
-	      b .  // do nothing
-	
-	/////////////////////////////////////////////////////////////////////////////
-	
-        .thumb_func
 dummy_handler:  
         b .  // do nothing
 
