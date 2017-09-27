aed57ecb528da9d092ad897de1d3a8de0b476b37
andernil <annil@stud.ntnu.no>
2017-09-27 13:38:23 +0200

    Added code for enabling GPIO inputs and interrupts

--- Exercise_2/gpio.c
+++ Exercise_2/gpio.c
@@ -11,14 +11,23 @@ void setupGPIO()
 	/*
 	 * TODO set input and output pins for the joystick 
 	 */
+    *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO;	/* enable GPIO clock */
+    *GPIO_PC_MODEL = 0x33333333; /*Enable inputs for GPIO pins 0-7*/
+    *GPIO_PC_DOUT = 0xFF;    /*Enable internal pullups for the input pins*/
 
+	*GPIO_PA_MODEH = 0x55555555;	/* set pins A8-15 as output */
+	*GPIO_PA_CTRL = 0x2;	/* set high drive strength */
+
+    /*
+        Enable interrupts for GPIO
+    */
+    *GPIO_EXTIPSELL = 0x22222222; /* Enable interrupts on all input pins */
+    *GPIO_EXTIFALL = *GPIO_EXTIRISE = *GPIO_IEN = 0xFF; /* Write 0xFF to enable interrupts, and enable interrupts on positive and negative slope */
 	/*
 	 * Example of HW access from C code: turn on joystick LEDs D4-D8 check 
 	 * efm32gg.h for other useful register definitions 
 	 */
-	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO;	/* enable GPIO clock */
-	*GPIO_PA_CTRL = 2;	/* set high drive strength */
-	*GPIO_PA_MODEH = 0x55555555;	/* set pins A8-15 as output */
 	*GPIO_PA_DOUT = 0x0700;	/* turn on LEDs D4-D8 (LEDs are active
 				 * low) */
 }
+
