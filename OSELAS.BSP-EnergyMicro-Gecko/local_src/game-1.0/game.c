d9e087426a38d69bc245adb87d9d3f7cf43a7cd6
Jon Andreas Melbye <jonandme@stud.ntnu.no>
2017-11-21 10:54:34 +0100

    Added .h file. Almost done with sigiohandler.

--- OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -9,14 +9,43 @@
 #include <signal.h>
 #include <fcntl.h>
 
+// need to include this to use the fb_copyarea struct
+#include <linux/fb.h>
+
+#include "game.h"
 
 FILE* driver;
 
+short board[BOARD_WIDTH*BOARD_HEIGHT];
+
+struct fb_copyarea head_rect;
+struct fb_copyarea tail_rect;
+struct fb_copyarea fruit_rect;
+
+snake snake_head;
+snake snake_tail;
+
+dir_list* unused_items;
 
 void sigio_handler(int signo)
 {
     printf("Signal nr.: %d\n", signo);
     printf("knapp: %d\n",fgetc(driver));
+    snake_head.dir = 
+    if (snake_tail.list==unused_items){
+      snake_head.list->next = malloc(sizeof(dir_list))
+      snake_head.list = snake_head.list->next;
+      snake_head.list->dir = snake_head.dir;
+      snake_head.list->count = 0;
+      snake_head.list->next = NULL;
+    } else {
+      snake_head.list->next = unused_items;
+      snake_head.list = unused_items;
+      unused_items = unused_items.next;
+      snake_head.list->dir = snake_head.dir;
+      snake_head.list->count = 0;
+      snake_head.list->next = NULL;
+    }
     
 }
 
@@ -30,7 +59,17 @@ int main(int argc, char *argv[])
 		printf("Driver error\n");
 		exit(EXIT_SUCCESS);
 	}
+	
+	// init: set start position
+	// init: place snake & set tail position
 
+	// init: set rect sizes
+	head_rect.width=3;
+	head_rect.height=3;
+	tail_rect.width=3;
+	tail_rect.height=3;
+	fruit_rect.width=3;
+	fruit_rect.height=3;
 
 	signal(SIGIO, &sigio_handler);
 	fcntl(fileno(driver), F_SETOWN, getpid());
---
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.h
@@ -0,0 +1,33 @@
+#ifndef GAME_H
+#define GAME_H
+
+// need to include this to use the fb_copyarea struct
+#include <linux/fb.h>
+
+#define BOARD_WIDTH 320
+#define BOARD_HEIGHT 240
+#define START_X ((BOARD_WIDTH)/2)
+#define START_Y ((BOARD_HEIGHT)/2)
+#define SNAKE_MIN_LENGTH 5
+#define SNAKE_MAX_LENGTH 100
+
+typedef struct {
+  short count;
+  char dir;
+  struct dir_list* next;
+} dir_list;
+
+typedef struct fb_copyarea fb_copyarea;
+
+typedef struct {
+  short* pos;
+  short x_pos;
+  short y_pos;
+  fb_copyarea* copyarea;
+  dir_list* list;
+  char move; // move when 0, counts down
+  char dir;  // 0 east, 1 south, 2 west, 3 north
+} snake;
+
+
+#endif
