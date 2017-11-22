9acab38d8fa31ef70b8b868f4fe25979c5031427
torgeile <torgeile@stud.ntnu.no>
2017-11-22 15:50:25 +0100

    Clear screen

--- OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -9,10 +9,10 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <string.h>
 
 // linux kernels
 #include <linux/fb.h>
-
 #include "game.h"
 
 FILE* driver;
@@ -23,13 +23,11 @@ static short* board;
 fb_copyarea head_rect;
 fb_copyarea tail_rect;
 fb_copyarea fruit_rect;
-
+fb_copyarea board_rect;
 static snake snake_head;
 static snake snake_tail;
 
 dir_list* unused_items = NULL;
-
-//static struct timer_list screen_timer;
 
 static int fbfd;
 
@@ -39,7 +37,7 @@ void sigio_handler(int signo)
     static int last_button_return = 4;
     int button_return;
     button_return = fgetc(driver);
-    printf("button_return: %d last_button_return %d \n", button_return,last_button_return);
+//    printf("button_return: %d last_button_return %d \n", button_return,last_button_return);
     if (button_return == last_button_return || button_return == 0){
 	timer_handler(0);
     }else{
@@ -137,41 +135,55 @@ void timer_handler(int signo){
   }
 }
 
+int clear_board(int i)
+{
+  
+  printf("Clearing board");
+  memset(board,0,BOARD_WIDTH*BOARD_HEIGHT*sizeof(short)); 
+  
+  board_rect.width = BOARD_WIDTH;
+  board_rect.height = BOARD_HEIGHT;
+  board_rect.dx = 0;
+  board_rect.dy = 0;
+  ioctl(fbfd,0x4680,&board_rect);
+  return 0;
+}
+
 int main(int argc, char *argv[])
 {
-	printf("Game starting, hello world\n");
+	printf("Game starting, hello snake world\n");
 	
 	driver = fopen("/dev/driver-gamepad", "rb");
 	timer = fopen("/dev/timer-module", "rb");
 
   fbfd = open("/dev/fb0", O_RDWR, 0);
-  board = (short*) mmap(NULL, BOARD_WIDTH * BOARD_HEIGHT, \
-                        PROT_READ|PROT_WRITE, MAP_SHARED, fbfd, 0);
+  board = (short*) mmap(NULL, BOARD_WIDTH * BOARD_HEIGHT, PROT_READ|PROT_WRITE, MAP_SHARED, fbfd, 0);
 
   if (driver == NULL) {
-		printf("Driver error\n");
-		exit(EXIT_SUCCESS);
-	}
-  // init: draw snake
-	head_rect.width = 1;
-	head_rect.height = 1;
-  head_rect.dy = START_Y;
-  int i;
-  for (i=0; i<SNAKE_MIN_LENGTH;i++){
-    head_rect.dx = START_X - SNAKE_MIN_LENGTH/2 + i;
-    ioctl(fbfd,0x4680,head_rect);
+    printf("Driver error\n");
+    exit(EXIT_SUCCESS);
   }
+
+  if (timer == NULL) {
+    printf("Timer error\n");
+    exit(EXIT_SUCCESS);
+  }
+  // init: draw snake
+  head_rect.width = 1;
+  head_rect.height = 1;
+  head_rect.dy = START_Y;
+  head_rect.dx = START_X + SNAKE_MIN_LENGTH/2;
 	// init: finish copyarea
-  tail_rect.dx = START_X - 2;
+  tail_rect.dx = START_X - SNAKE_MIN_LENGTH/2;
   tail_rect.dy = START_Y;
-	tail_rect.width = 1;
-	tail_rect.height = 1;
-	fruit_rect.width = 3;
-	fruit_rect.height = 3;
+  tail_rect.width = 1;
+  tail_rect.height = 1;
+  fruit_rect.width = 3;
+  fruit_rect.height = 3;
 
 	// init: set tail & head position
   snake_head.list = (void*) malloc(sizeof(dir_list));
-  snake_head.list->count = 5;
+  snake_head.list->count = SNAKE_MIN_LENGTH-1;
   snake_head.list->dir = 4;
   snake_head.list->next = NULL;
   snake_tail.list = snake_head.list;
@@ -181,23 +193,21 @@ int main(int argc, char *argv[])
   snake_tail.dir = 4;
   snake_head.copyarea = &head_rect;
   snake_tail.copyarea = &tail_rect;
-  snake_head.pos = board + head_rect.dx + \
-                   head_rect.dy * BOARD_WIDTH;
-  snake_tail.pos = board + tail_rect.dx +
-                   tail_rect.dy * BOARD_WIDTH;
+  snake_head.pos = board + head_rect.dx + head_rect.dy * BOARD_WIDTH;
+  snake_tail.pos = board + tail_rect.dx + tail_rect.dy * BOARD_WIDTH;
 
-	signal(SIGIO, &sigio_handler);
-	fcntl(fileno(driver), F_SETOWN, getpid());
-	long oflags = fcntl(fileno(driver), F_GETFL);
-    	fcntl(fileno(driver), F_SETFL, oflags | FASYNC);
+  clear_board(1);
 
-	fcntl(fileno(timer), F_SETOWN, getpid());
-	oflags = fcntl(fileno(timer), F_GETFL);
-    	fcntl(fileno(timer), F_SETFL, oflags | FASYNC);
+  signal(SIGIO, &sigio_handler);
+  fcntl(fileno(driver), F_SETOWN, getpid());
+  long oflags = fcntl(fileno(driver), F_GETFL);
+  fcntl(fileno(driver), F_SETFL, oflags | FASYNC);
+  fcntl(fileno(timer), F_SETOWN, getpid());
+  oflags = fcntl(fileno(timer), F_GETFL);
+  fcntl(fileno(timer), F_SETFL, oflags | FASYNC);
 
-
+  
   while(1){
-	    printf("\n");
 	    pause();
 	};
 
